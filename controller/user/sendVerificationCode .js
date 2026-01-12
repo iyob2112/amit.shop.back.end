58354a8633f21cf793bb46bad70840b607e31315
iyob2112 <iyobmw01@gmail.com>
2026-01-12 16:35:22 +0300

    bcryptjs

--- controller/user/sendVerificationCode .js
+++ controller/user/sendVerificationCode .js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/userModel");
 const nodemailer = require("nodemailer");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 // Transporter for nodemailer
 const transporter = nodemailer.createTransport({
