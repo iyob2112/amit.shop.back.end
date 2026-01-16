b09d5e969220f75a12d2fc1148eb69c6f81e1808
iyob2112 <148183401+iyob2112@users.noreply.github.com>
2026-01-16 17:29:00 +0300

    Refactor token retrieval in protect middleware
    Refactor token retrieval logic to prioritize authorization header over cookies.

--- controller/authController.js
+++ controller/authController.js
@@ -44,7 +44,17 @@ const User = require('../models/userModel')
 
 exports.protect = async (req, res, next) => {
     try{
-        const token = req.cookies?.token
+        // const token = req.cookies?.token
+                let token;
+
+if (
+  req.headers.authorization &&
+  req.headers.authorization.startsWith("Bearer")
+) {
+  token = req.headers.authorization.split(" ")[1];
+} else if (req.cookies?.token) {
+  token = req.cookies.token;
+}
 
         console.log("token",token)
         if(!token){
