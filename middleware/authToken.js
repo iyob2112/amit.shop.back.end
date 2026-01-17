272fcb0bc402075ef7355530d7575f07902b6796
iyob2112 <iyobmw01@gmail.com>
2026-01-17 12:42:25 +0300

    end1

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -2,68 +2,68 @@ const jwt = require('jsonwebtoken');
 
 async function authToken(req, res, next) {
 
-     const authHeader = req.headers.authorization;
-     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+  //    const authHeader = req.headers.authorization;
+  //    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  //   return res.status(401).json({ message: "Unauthorized" });
+  // }
 
-  const token = authHeader.split(" ")[1];
+  // const token = authHeader.split(" ")[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+  // try {
+  //   const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+  //   req.user = decoded;
+  //   next();
+  // } catch (err) {
+  //   return res.status(401).json({ message: "Invalid token" });
+  // }
 // };
-//     try {
-//         // const token = req.cookies?.token;
-//           let token;
+    try {
+        // const token = req.cookies?.token;
+          let token;
 
-//     // Read token from HEADER or COOKIE
-//     if (
-//       req.headers.authorization &&
-//       req.headers.authorization.startsWith("Bearer")
-//     ) {
-//       token = req.headers.authorization.split(" ")[1];
-//     } else if (req.cookies?.token) {
-//       token = req.cookies.token;
-//     }
+    // Read token from HEADER or COOKIE
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies?.token) {
+      token = req.cookies.token;
+    }
 
-//         console.log("Token received:", token);
-//         if (!token) {
-//             return res.status(401).json({
-//                 message: "Please Login...!",
-//                 error: true,
-//                 success: false
-//             });
-//         }
+        console.log("Token received:", token);
+        if (!token) {
+            return res.status(401).json({
+                message: "Please Login...!",
+                error: true,
+                success: false
+            });
+        }
 
-//         jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
-//             if (err) {
-//                 console.error("JWT Verification Error:", err);
-//                 return res.status(401).json({
-//                     message: "Invalid or Expired Token",
-//                     error: true,
-//                     success: false
-//                 });
-//             }
+        jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+            if (err) {
+                console.error("JWT Verification Error:", err);
+                return res.status(401).json({
+                    message: "Invalid or Expired Token",
+                    error: true,
+                    success: false
+                });
+            }
 
-//             console.log("Decoded Token Data:", decoded);
-//             req.userId = decoded._id || decoded.id;//yoooooooooo
+            console.log("Decoded Token Data:", decoded);
+            req.userId = decoded._id || decoded.id;//yoooooooooo
 
-//             next();
-//         });
+            next();
+        });
 
-//     } catch (err) {
-//         console.error("Auth Middleware Error:", err);
-//         res.status(500).json({
-//             message: "Internal Server Error",
-//             error: true,
-//             success: false
-//         });
-//     }
+    } catch (err) {
+        console.error("Auth Middleware Error:", err);
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: true,
+            success: false
+        });
+    }
 }
 
 module.exports = authToken;
